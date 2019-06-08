9ab9c900d735f4826b2d5d381f9853d59d3c260a
jonjon1025 <jonathanq1025@yahoo.com>
2019-06-08 23:35:09 +0000

    new

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Splurty
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+  
+  config.assets.paths << "#{rails.root}/app/assets/fonts"
+
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
+
+
+
+
   end
+
 end
